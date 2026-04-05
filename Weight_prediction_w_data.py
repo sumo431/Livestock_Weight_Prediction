885c0ae8c574de8b873e5300b1fa8f70a77ea185
sumo431 <sokada1@csuchico.edu>
2026-04-04 23:20:35 -0700

    complete the prediction code

--- Weight_prediction_w_data.py
+++ Weight_prediction_w_data.py
@@ -1,7 +1,32 @@
+from statistics import LinearRegression
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
 
+
+# Load the dataset
 df = pd.read_csv('')
 
-X = df[['animal_type', 'age', 'gender', 'variety']]
+#devide X and Y as features and target variable
+X = df[['animal_type', 'age', 'sex', 'variety']]
+Y = df['weight']
+
+# Split the dataset into training and testing sets
+ct = ColumnTransformer(transformers=[
+    ('encoder', OneHotEncoder(), ['animal_type', 'sex', 'variety'])
+], remainder='passthrough')
+
+X = ct.fit_transform(X)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, Y_train)
+
+predictions = model.predict(X_test)
+print(predictions)
+
+
